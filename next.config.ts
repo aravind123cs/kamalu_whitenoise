098f0e3bbfc9c5ed9dab90406e7461a55b317ef3
Aravind <aravind@applab.qa>
2025-05-30 17:56:24 +0530

    animation added

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -7,17 +7,7 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  }
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,30 @@
-import AiAssistant from "@/components/AiAssistant";
+"use client"
 import SoundPlayer from "@/components/SoundPlayer";
 import { sounds } from "@/lib/sounds";
-import { Separator } from "@/components/ui/separator";
+import { Typewriter } from 'react-simple-typewriter'
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-4 md:p-8 lg:p-12 bg-background selection:bg-primary/30 selection:text-primary-foreground">
       <div className="w-full max-w-lg space-y-10">
         <header className="text-center pt-8">
-          <h1 className="text-5xl md:text-6xl font-bold text-primary-foreground tracking-tight">
-            Kamalu's White noise
+          <h1 className="text-5xl md:text-4xl font-bold text-primary-foreground tracking-tight">
+            <Typewriter
+              words={["Kamalu's White noise", "Relax. Focus. Sleep."]}
+              loop={true}
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1500}
+            />
           </h1>
           <p className="text-xl text-muted-foreground mt-3">
             Soothing sounds for peaceful sleep.
           </p>
         </header>
-
         <SoundPlayer sounds={sounds} />
-
-
       </div>
     </main>
   );
